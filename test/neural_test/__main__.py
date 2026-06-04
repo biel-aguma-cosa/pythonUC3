d4614581f808ac7bad1f2e0346ea283abb01b3f7
biel-aguma-cosa <49573010+biel-aguma-cosa@users.noreply.github.com>
2026-06-03 21:46:04 -0300

    ai thing started

--- test/neural_test/__main__.py
+++ test/neural_test/__main__.py
@@ -1,4 +1,4 @@
-import numpy as np, pygame, time, threading
+import numpy as np, pygame, time, threading, random
 
 WIDTH, HEIGHT = 600,400
 
@@ -11,47 +11,99 @@ running = True
 t0 = 0
 dt = 0
 
-dots = np.array([])
-it = 0
+GRID0 = np.array([[0 for i in range(20)] for i in range(30)])
 
-def raying():
-    global dots, it
-    ray_num = 7
-    fov = np.deg2rad(90)
-    center = (300,200)
-    direction = np.array([(np.cos(fov/ray_num-fov/2), np.sin(fov/ray_num-fov/2)) for i in range(ray_num)])
-    rays = np.array([np.array(center,np.float64) for i in range(ray_num)])
-    while it < 30:
-        rays += direction
-        np.append(dots,rays)
-        time.sleep(0.1)
-        it += 0.1
-threading.Thread(target=raying).start()
+for i in range(30):
+    GRID0[ i][ 0] = 1
+    GRID0[ i][19] = 1
+for i in range(20):
+    GRID0[ 0][ i] = 1
+    GRID0[29][ i] = 1
 
-#try:
+def reset(walls=30):
+    global GRID, center
+    x, y = 0, 0
+    GRID = GRID0.copy()
+    for i in range(walls):
+        GRID[random.randint(0,29),random.randint(0,19)] = 1
+    GRID[random.randint(0,29),random.randint(0,19)] = 5
+    while GRID[x,y] != 0:
+        x = random.randint(0,29)
+        y = random.randint(0,19)
+    center = np.array((x*20+10,y*20+10))
+reset()
+
+rays = []
+fov  = np.deg2rad(97)
+ray_num = 7
+rays = np.array([np.array(center,np.float64) for i in range(ray_num)])
+angle = 0
+
+
 while running:
-    mouse = np.array(pygame.mouse.get_pos())-(300,200)
+    key = pygame.key.get_pressed()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     screen.fill('grey')
 
-    pygame.draw.circle(screen,'black',(300,200),5,1)
+    angle += -np.deg2rad((key[pygame.K_RIGHT] - key[pygame.K_LEFT])*5)
 
+    input_mov = np.array((key[pygame.K_d] - key[pygame.K_a],key[pygame.K_s] - key[pygame.K_w]))
+    input_mov = input_mov*4/(np.linalg.norm(input_mov)) if np.linalg.norm(input_mov) != 0 else input_mov*0
 
-    n = np.linalg.norm(mouse)
+    theta = np.atan2(input_mov[0],input_mov[1])
 
-    n2= np.acos(mouse[0]/n)/np.pi
-    print(n2)
+    px = -(input_mov[0] * np.cos(-angle) - input_mov[1] * np.sin(-angle))
+    py = -(input_mov[0] * np.sin(-angle) + input_mov[1] * np.cos(-angle))
 
-    print(dots)
-    for dot in tuple(dots):
-        pygame.draw.circle(screen,'red',dot,2)
+    next_pos = center[0]+int(px),center[1]+int(px)
+    if GRID[next_pos[0]//20,center[1]//20] == 0:
+        center[0] += int(px)
+    elif GRID[next_pos[0]//20,center[1]//20] == 5:
+        reset()
+        #reward 
+    else:
+        center[0] -= int(px)
+        #punish
 
-    pygame.draw.line(screen,'orange',(300,200),((mouse[0]*32/n)+300,(mouse[1]*32/n)+200),3)
+    if GRID[center[0]//20,next_pos[1]//20] == 0:
+        center[1] += int(py)
+    elif GRID[next_pos[0]//20,center[1]//20] == 5:
+        reset()
+        #reward 
+    else:
+        center[1] -= int(py)
+        # punish
+
+    direction = np.array([(np.cos((-angle+i*(fov/ray_num))+fov/2), np.sin((-angle+i*(fov/ray_num))+fov/2)) for i in range(ray_num)])
+    rays /= (rays+0.00000000000001)
+    rays *= center
+    for i in range(400):
+        clean = GRID[np.int64(rays[:,0]//20),np.int64(rays[:,1]//20)] == 0
+        if i % 20 == 0:
+            for ray in tuple(rays):
+                #print((ray//20)*20)
+                if GRID[int(ray[0]//20)][int(ray[1]//20)] == 1:
+                    pygame.draw.rect(screen,'red',((ray[0]//20)*20,(ray[1]//20)*20,19,19))
+                elif GRID[int(ray[0]//20)][int(ray[1]//20)] == 5:
+                    #reward
+                    pygame.draw.rect(screen,'blue',((ray[0]//20)*20,(ray[1]//20)*20,19,19))
+                else:
+                    pygame.draw.rect(screen,'green',((ray[0]//20)*20,(ray[1]//20)*20,19,19))
+        rays[clean] += direction[clean]*1
+    hit   = GRID[np.int64(rays[:,0]//20),np.int64(rays[:,1]//20)] == 1
+    result = np.zeros(ray_num)
+    result[ hit] = 1 - np.linalg.norm(rays[hit] - center, axis=1)/400
+    print(GRID[np.int64(rays[:,0]//20),np.int64(rays[:,1]//20)])
+    print(result)
+
+    for ray in tuple(rays.copy()):
+        pygame.draw.line(screen,'red',center,ray)
+    #pygame.draw.line(screen,'blue',center,((mouse[0]*32/(n+0.00000000000001))+center[0],(mouse[1]*32/n+0.0000000000000001)+center[1]),1)
+    pygame.draw.circle(screen,'black',center,5,1)
 
     pygame.display.flip()
     dt = clock.tick(30)/1000
     t0 += dt
-#except:
-#    it = 80
+running = False
--- test/neural_test/mind.py
+++ test/neural_test/mind.py
@@ -4,15 +4,11 @@ def relu(x):
     if x > 0:
         return
 
-# up down left right | rotate_left rotate_right
-# i1 h1 h1 o1
-# i2 h2 h2 o2
-# i3 h3 h3 o3
-# i4 h4 h4 o4
-# i5 h5 h5 o5
-# i6       06
-# i7
-# i8
+#
+#
+#
+#
+#
 
 ray_num = 7
 angle = np.deg2rad(90)
