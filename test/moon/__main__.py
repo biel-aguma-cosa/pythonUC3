3e52969831386ef5139da49608554c906938c2ce
biel-aguma-cosa <49573010+biel-aguma-cosa@users.noreply.github.com>
2026-07-08 18:11:11 -0300

    moon fail

--- test/moon/__main__.py
+++ test/moon/__main__.py
@@ -4,13 +4,16 @@ from scipy.constants import G
 pygame.init()
 
 clock = pygame.time.Clock()
-tps = 60
+tps = 0.1
 dt = 1/tps
 
+compact = 1000
 def km(n):
-    return n * 2000
+    global compact
+    return n * compact
 def un_km(n):
-    return n / 2000
+    global compact
+    return n / compact
 
 class screen:
     w = 1200
@@ -18,22 +21,21 @@ class screen:
     surface = pygame.display.set_mode((w,h))
 
 class moon:
-    pos = np.array([385600,100],dtype=np.float64)
-    spd = np.array([3683,0],dtype=np.float64)
+    pos = np.array([600,0],dtype=np.float64)
+    spd = np.array([0,3683],dtype=np.float64)
     w = 7.3*10**22
-    r = 1.740
+    r = 1740
 class earth:
-    pos = np.array([600,400],dtype=np.float64)
+    pos = np.array([0,0],dtype=np.float64)
     spd = np.array([0,0],dtype=np.float64)
     w = 5.972*10**24
-    r = 6.380
+    r = 6380
 
 V = moon.pos-earth.pos
 D = np.linalg.norm(V)
-F = G*earth.r*moon.r/D if D else 0
+F = G*earth.w*moon.w/D if D else np.float64(0)
 
-moon .spd += F*V/D
-earth.spd += F*V/D
+C = np.array([600,400],dtype=np.float64)
 
 running = True
 while running:
@@ -42,10 +44,23 @@ while running:
             running = False
     screen.surface.fill('black')
     
-    pygame.draw.circle(screen.surface,'blue',earth.pos,100)
-    pygame.draw.circle(screen.surface,'white',moon.pos,10)
+    V = moon.pos-earth.pos
+    D = np.linalg.norm(V)
+    F = G*earth.w*moon.w/D if D else np.float64(0)
+
+    print(V, D, F)
+
+    moon .spd += (F*V/np.pow(D,2))/dt
+    earth.spd += (F*V/np.pow(D,2))/dt
+
+    moon .pos += moon .spd
+    earth.pos += earth.spd
+
+    print(un_km(earth.pos),un_km(moon.pos))
+    pygame.draw.circle(screen.surface,'blue' ,C,un_km(earth.r))
+    pygame.draw.circle(screen.surface,'white',un_km(moon.pos)-un_km(earth.r)+C,un_km(moon.r))
 
     pygame.display.flip()
-    clock.tick(tps)
+    dt = clock.tick(60)/1000
 
 print(G)
